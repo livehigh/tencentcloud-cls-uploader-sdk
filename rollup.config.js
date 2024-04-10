db15fe3f14e6edb65b0233018b9e739d465ace6e
chrisftian <chrisftian@tencent.com>
2024-04-10 20:57:42 +0800

    fix: 支持打包压缩版

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 
 const extensions = ['.js', '.ts'];
 
@@ -79,6 +80,35 @@ export default [
         mainFields: ['browser'],
       }),
       commonjs(),
+    ],
+    onwarn: (msg, warn) => {
+      if (/Circular dependency/.test(msg)) return;
+      warn(msg);
+    },
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      format: 'umd',
+      file: 'dist/cls.min.js',
+      name: 'ClsClient',
+    },
+    external: [],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      babel({
+        exclude: ['node_modules/**', 'src/proto/cls.js'],
+        extensions,
+      }),
+      json(),
+      resolve({
+        preferBuiltins: true, //这一句是重点
+        mainFields: ['browser'],
+      }),
+      commonjs(),
+      terser(),
     ],
     onwarn: (msg, warn) => {
       if (/Circular dependency/.test(msg)) return;
